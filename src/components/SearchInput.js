a816e02a716c50bc54874df4537c5ffe64535d17
unknown <hamzahmitou@gmail.com>
2019-09-24 18:01:58 +0300

    SearchInput component

---
+++ src/components/SearchInput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/SearchInput.css';
+
+
+const SearchInput = () => {
+    return <input className="search-input" type="text" name="countrySearch" placeholder="Search for a country"/>;
+}
+
+export default SearchInput;
