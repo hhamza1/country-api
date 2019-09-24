e13fbd9c3bbfc64dccb1bf5422f09ea6b0b52779
unknown <hamzahmitou@gmail.com>
2019-09-24 17:00:31 +0300

    Added Header container

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from './Header';
 import './style/App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Header />
   );
 }
 
