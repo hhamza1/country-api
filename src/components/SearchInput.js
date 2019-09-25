af2ebf0945498c6436345c5f8259574b0a28286f
unknown <hamzahmitou@gmail.com>
2019-09-25 16:28:25 +0300

    Added magnifier icon to input

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -3,7 +3,17 @@ import './styles/SearchInput.css';
 
 
 const SearchInput = () => {
-    return <input className="search-input" type="text" name="countrySearch" placeholder="Search for a country"/>;
+    return (
+        <div className="input-group">
+            <input 
+            className="search-input" 
+            type="text" 
+            name="countrySearch" 
+            placeholder="Search for a country">
+            </input>
+            <span className="lnr lnr-magnifier"></span>
+        </div>
+    );
 }
 
 export default SearchInput;
