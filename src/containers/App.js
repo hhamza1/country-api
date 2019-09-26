bae33f4437d293b8ce24dfab9e29eae62d097ffa
unknown <hamzahmitou@gmail.com>
2019-09-26 17:31:03 +0300

    Added filter dropdown

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import SearchInput from '../components/SearchInput';
+import Filter from '../components/Filter';
 import './style/App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <SearchInput />
+      <Filter />
     </div>
   );
 }
