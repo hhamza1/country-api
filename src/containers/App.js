4d5b0fbca73ca3539439de5669559b80f5bba9ef
unknown <hamzahmitou@gmail.com>
2019-10-01 17:21:36 +0300

    Initiate state & fetch api

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './Header';
 import SearchInput from '../components/SearchInput';
-import Country from '../components/Country';
+import CountryList from '../containers/CountryList';
 import Filter from '../components/Filter';
 import './style/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <SearchInput />
-      <Filter />
-      <Country />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: ['Germany','Morocco','Turkey','Japan'],
+      selecteCountry: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({countries : result})
+        });
+  }
+
+
+
+  render() {
+    const {countries} = this.state;
+    return (
+      <div className="App">
+        <Header />
+        <SearchInput />
+        <Filter />
+        <CountryList countries={countries} />
+      </div>
+    );
+  }
 }
 
 export default App;
