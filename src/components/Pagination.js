6ce894ccf995a7735bd23287234a882a29023c6f
unknown <hamzahmitou@gmail.com>
2019-10-01 20:53:33 +0300

    Added pagination to the app

---
+++ src/components/Pagination.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles/Pagination.css';
+
+const Pagination = ({countryPerPage, totalCountries, paginate}) => {
+    const pageNumbers = [];
+    for(let i=1; i <= Math.ceil(totalCountries/countryPerPage); i++ ) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {
+                    pageNumbers.map(number => (
+                        <li key={number}>
+                            <a onClick={() => paginate(number)} href="!#" className="page-link">
+                                {number}
+                            </a>
+                        </li>
+                    ))
+                }
+            </ul>   
+        </nav>
+    );
+
+}
+
+export default Pagination;
