57aa8f54a296cfe4645f873ae3c958df45e7fc4e
unknown <hamzahmitou@gmail.com>
2019-10-01 15:12:22 +0300

    Added the country component to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import SearchInput from '../components/SearchInput';
+import Country from '../components/Country';
 import Filter from '../components/Filter';
 import './style/App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <SearchInput />
       <Filter />
+      <Country />
     </div>
   );
 }
