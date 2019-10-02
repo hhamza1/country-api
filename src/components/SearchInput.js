297c1ef7b92dd3c4c2cc22f97c0136c174990c6c
unknown <hamzahmitou@gmail.com>
2019-10-02 17:27:48 +0300

    Add onChange event

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './styles/SearchInput.css';
 
 
-const SearchInput = () => {
+const SearchInput = ({searchCountry}) => {
     return (
         <div className="input-group">
             <input 
             className="search-input" 
             type="text" 
             name="countrySearch" 
-            placeholder="Search for a country">
+            placeholder="Search for a country"
+            onChange={searchCountry}
+            >
             </input>
             <span className="lnr lnr-magnifier"></span>
         </div>
