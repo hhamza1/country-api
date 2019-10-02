c472b6ca91631439fb17b121c054100f09d505b6
unknown <hamzahmitou@gmail.com>
2019-10-02 17:27:36 +0300

    Changed filter into a functional component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,36 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './styles/Filter.css';
 
 
-class Filter extends Component {
-    constructor() {
-        super();
-        this.state = {
-            dropdownElement : "Filter by Region"
-        };
-    }
-
-    render() {
-        const {dropdownElement} = this.state;
-        const selectFilter = (event) => {
-            this.setState({dropdownElement : event.target.id});
-        }
-
+const Filter = ({selectRegion, currentRegion}) => {
         return (
             <div className="filter dropdown">
-                <div className="dropdown" tabIndex="0">{dropdownElement}</div>
+                <div className="dropdown" tabIndex="0">{currentRegion===''? "Filter by Region": currentRegion}</div>
                 <div tabIndex="-1" className="downarrow"></div>
                 <div>
                     <ul>
-                        <li id="Africa" className="filter-child" onClick={selectFilter} >Africa</li>
-                        <li id="America" className="filter-child" onClick={selectFilter} >America</li>
-                        <li id="Asia" className="filter-child" onClick={selectFilter} >Asia</li>
-                        <li id="Europe" className="filter-child" onClick={selectFilter} >Europe</li>
-                        <li id="Oceania" className="filter-child" onClick={selectFilter} >Oceania</li>
+                        <li id="Africa" className="filter-child" onClick={selectRegion} >Africa</li>
+                        <li id="America" className="filter-child" onClick={selectRegion} >America</li>
+                        <li id="Asia" className="filter-child" onClick={selectRegion} >Asia</li>
+                        <li id="Europe" className="filter-child" onClick={selectRegion} >Europe</li>
+                        <li id="Oceania" className="filter-child" onClick={selectRegion} >Oceania</li>
                     </ul>
                 </div>
         </div>);
     }
-}
+
 
 export default Filter;
