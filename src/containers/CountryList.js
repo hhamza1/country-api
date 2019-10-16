65b4f1fb66c39bb8965ffe176fc04ffbc64b3ae4
unknown <hamzahmitou@gmail.com>
2019-10-16 16:38:47 +0300

    Added react-router Link component

--- src/containers/CountryList.js
+++ src/containers/CountryList.js
@@ -1,29 +1,38 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Country from '../components/Country';
 import './style/CountryList.css';
 
 
 
-const CountryList = ({countries, darkMod}) => {
-
+const CountryList = ({countries, darkMod, selectedCountry}) => {
     return(
-        <div className="country-list">
-            {   
-                countries.map((country, key) => {
-                    return(
-                        <Country 
-                            key={key} 
-                            name={country.name} 
-                            flag={country.flag}
-                            capital={country.capital}
-                            population={country.population}
-                            region={country.region}
-                            darkMod={darkMod}
-                        />
-                    )
-                })
-            }
-        </div>
+            <div className="country-list">
+                
+                {
+                    countries.map((country, key) => {
+                        return(
+                                <Link key={key} style={{ textDecoration: 'none' }}  to={`/${country.name}`}>
+                                    <Country
+                                        country={country}
+                                        name={country.name} 
+                                        flag={country.flag}
+                                        capital={country.capital}
+                                        population={country.population}
+                                        region={country.region}
+                                        darkMod={darkMod}
+                                        selectedCountry={selectedCountry}
+                                    />
+                                </Link>
+                        );
+                    })
+                    
+                }
+                    
+                
+            </div>
+
+             
     );
 }
 
