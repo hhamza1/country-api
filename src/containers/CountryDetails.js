e87b247c2cd86fcf1afd579a40364785e09719c1
unknown <hamzahmitou@gmail.com>
2019-10-17 15:51:38 +0300

    Added country details

--- src/containers/CountryDetails.js
+++ src/containers/CountryDetails.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './style/CountryDetails.css';
 
 
 const CountryDetails = ({selectedCountry}) => {
     return(
-        <h1>
-            {selectedCountry.name}
-        </h1>
+        <div className="selected-country-details">
+            <button>
+                Back
+            </button>
+            <div className="selected-country">
+                <div className="selected-country-flag">
+                    <img src={selectedCountry.flag} alt={selectedCountry.name}/>
+                </div>
+                <div className="selected-details">
+                    <h1>{selectedCountry.name}</h1>
+                </div>
+            </div>
+        </div>
     );
 }
 
