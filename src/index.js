fc730bc80c15915746fbd10005a100dbe7961c6c
unknown <hamzahmitou@gmail.com>
2019-11-10 16:01:39 +0300

    Add combineReducers method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React  from 'react';
 import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
 import './index.css';
 import App from './containers/App';
+import {setCountry, setRegion} from './reducers/reducers';
 
 
-render(<App/>, document.getElementById('root'));
+const rootReducer = combineReducers({setCountry, setRegion});
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
 
 
