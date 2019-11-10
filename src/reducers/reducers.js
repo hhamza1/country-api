0126aa28bea1e5c16fcd3bd562c2f28aeafc2c03
unknown <hamzahmitou@gmail.com>
2019-11-10 16:03:34 +0300

    Add reducer to select region

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,14 +1,24 @@
-import { CHANGE_SEARCH_FIELD } from '../constants/constants';
+import { CHANGE_SEARCH_FIELD, SELECT_REGION } from '../constants/constants';
 
 const initialState = {
-    searchField : ''
+    searchField : '',
+    filteredRegion : ''
 }
 
 export const setCountry = (state=initialState, action={}) => {
     switch(action.type) {
         case CHANGE_SEARCH_FIELD:
-            return {...state, searchField: action.payload};
+            return Object.assign({},state, {searchField: action.payload});
         default: 
+            return state;
+    }
+}
+
+export const setRegion = (state=initialState, action={}) => {
+    switch(action.type) {
+        case SELECT_REGION:
+            return Object.assign({}, state, {filteredRegion: action.payload});
+        default:
             return state;
     }
 }
