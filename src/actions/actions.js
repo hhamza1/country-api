8e7fff042456988517c183fbcd371da6aa0cc864
unknown <hamzahmitou@gmail.com>
2019-11-10 16:01:52 +0300

    Add filter region action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,11 @@
-import { CHANGE_SEARCH_FIELD } from '../constants/constants';
+import { CHANGE_SEARCH_FIELD, SELECT_REGION } from '../constants/constants';
 
 export const  setSearchField = (text) =>({
     type: CHANGE_SEARCH_FIELD,
+    payload: text
+});
+
+export const setFilteredRegion = (text) => ({
+    type: SELECT_REGION,
     payload: text
 });
