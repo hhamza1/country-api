c3812f46628aa80fc4480fe9b4c735944a8c6d44
unknown <hamzahmitou@gmail.com>
2019-11-12 17:00:25 +0300

    Fixing accessibility

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -5,7 +5,8 @@ import './styles/SearchInput.css';
 const SearchInput = ({searchCountry, darkMod}) => {
     return (
         <div className="input-group">
-            <input 
+            <input
+            aria-label="Search for country"
             className={darkMod === false ? "search-input" : "search-input search-input__dark"}
             type="text" 
             name="countrySearch" 
