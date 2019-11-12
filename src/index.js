8677ee326dca229718558f3091a5cfe8d9cd1d5e
unknown <hamzahmitou@gmail.com>
2019-11-12 22:15:07 +0300

    combine changeTheme reducer into root

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import thunkMiddleware from 'redux-thunk';
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import './index.css';
 import App from './containers/App';
-import {setCountry, setRegion, requestCountries, selectCountry} from './reducers/reducers';
+import {setCountry, setRegion, requestCountries, selectCountry, changeTheme} from './reducers/reducers';
 
 
-const rootReducer = combineReducers({setCountry, setRegion, requestCountries, selectCountry});
+const rootReducer = combineReducers({setCountry, setRegion, requestCountries, selectCountry, changeTheme});
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
