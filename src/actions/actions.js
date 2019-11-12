1a86fa18b9821b7b1c76f48f57873727517b2f23
unknown <hamzahmitou@gmail.com>
2019-11-12 15:06:22 +0300

    Fetch API action added

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,13 @@
-import { CHANGE_SEARCH_FIELD, SELECT_REGION } from '../constants/constants';
+import { 
+    CHANGE_SEARCH_FIELD, 
+    SELECT_REGION,
+    REQUEST_COUNTRIES_PENDING,
+    REQUEST_COUNTRIES_SUCCESS,
+    REQUEST_COUNTRIES_FAIL
+
+} from '../constants/index';
+
+import * as axios from 'axios';
 
 export const  setSearchField = (text) =>({
     type: CHANGE_SEARCH_FIELD,
@@ -9,3 +18,13 @@ export const setFilteredRegion = (text) => ({
     type: SELECT_REGION,
     payload: text
 });
+
+export const requestCountries = dispatch => {
+    dispatch({type: REQUEST_COUNTRIES_PENDING});
+    axios({
+        method: 'get',
+        url: 'https://restcountries.eu/rest/v2/all'
+      })
+        .then(res => dispatch({type: REQUEST_COUNTRIES_SUCCESS, payload: res}))
+        .catch(error => dispatch({type: REQUEST_COUNTRIES_FAIL, payload: error}));
+}
